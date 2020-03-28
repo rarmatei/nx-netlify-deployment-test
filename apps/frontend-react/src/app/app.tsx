3b4ea1ba8d46c8e3750832aab5be3fb2f1a75bd2
Rares Matei <matei.rar@gmail.com>
2020-03-28 13:22:22 +0000

    update react

--- apps/frontend-react/src/app/app.tsx
+++ apps/frontend-react/src/app/app.tsx
@@ -15,7 +15,7 @@ export const App = () => {
     <div className="app">
       <header className="flex">
         <Logo width="75" height="75" />
-        <h1>Welcome to frontend-react!</h1>
+        <h1>Welcome to frontend-react updated!</h1>
       </header>
       <main>
         <h2>Resources &amp; Tools</h2>
